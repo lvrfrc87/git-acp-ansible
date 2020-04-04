63654c026db721d5ee22a9a56e272df0366d5838
Ansible Test Runner <noreply@example.com>
2020-04-04 18:35:24 +0200

    add descriptions

--- setup.py
+++ setup.py
@@ -4,13 +4,18 @@ from setuptools import setup, find_packages
 with open("requirements.txt", "r") as fs:
     reqs = [r for r in fs.read().splitlines() if (len(r) > 0 and not r.startswith("#"))]
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="git-acp-ansible",
     version='1.0.0',
-    packages=find_packages(exclude=("test*"),
+    packages=find_packages(exclude=("test*")),
     author="Federico Olivieri",
     author_email="lvrfrc87@gmail.com",
     description="Ansible module for git add/commit/push operations.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     classifiers=[
         'Topic :: Utilities',
         'Programming Language :: Python',
