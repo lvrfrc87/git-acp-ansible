7a3b3f3e99631ba858c7c7892fa5f6c22999b795
Federico87 <lvrfrc87@gmail.com>
2020-07-17 13:16:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="git-acp-ansible",
-    version='1.0.7',
+    version='1.0.8',
     packages=find_packages(exclude=("test*", "venv")),
     author="Federico Olivieri",
     author_email="lvrfrc87@gmail.com",
