cfd2d858ee4f2f11d1be63ffbe149cd535ffa9ac
Federico Olivieri <lvrfrc87@gmail.com>
2020-08-26 12:56:31 +0200

    Update version and remove python2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="git-acp-ansible",
-    version='1.0.8',
+    version='1.0.9',
     packages=find_packages(exclude=("test*", "venv")),
     author="Federico Olivieri",
     author_email="lvrfrc87@gmail.com",
