5ff3bb66b2910b55e52fa61510360200ebc06dbe
Federico Olivieri <lvrfrc87@gmail.com>
2020-08-26 12:56:54 +0200

    remove python2 support

--- setup.py
+++ setup.py
@@ -18,9 +18,6 @@ setup(
     long_description_content_type="text/markdown",
     classifiers=[
         'Topic :: Utilities',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
