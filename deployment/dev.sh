b289235add8609a8af8f57e94ba938465f625943
lvrfrc87 <federico.olivieri@networktocode.com>
2021-11-05 22:31:15 +0100

    initial draft of code refactoring class based

--- deployment/dev.sh
+++ deployment/dev.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-ansible-test sanity --docker -v plugins/modules/ && \
+cd ../ansible_collections/lvrfrc87/git_acp/ && \
+ansible-test sanity --docker -v plugins/ && \
 ansible-test integration -v --docker
 # ansible-lint tests/integration/targets/git_acp/tasks/main.yml
