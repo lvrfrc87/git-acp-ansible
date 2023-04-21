443817622b3cb438b52acfb5f64bd72c94129e88
lvrfrc87 <federico.olivieri@networktocode.com>
2023-04-21 22:30:53 +0200

    add 2.1.0 release

--- deployment/master.sh
+++ deployment/master.sh
@@ -2,4 +2,4 @@
 cd ../ansible_collections/lvrfrc87/git_acp/ && \
 ansible-galaxy collection build && \
 ansible-galaxy collection install lvrfrc87-git_acp-2.1.0.tar.gz -p ./tests/install/ && \
-ansible-galaxy collection publish ./lvrfrc87-git_acp-2.1.0.tar.gz --token=$GALAXY_TOKEN
+ansible-galaxy collection publish ./lvrfrc87-git_acp-2.1.0.tar.gz --token=e7460647d7746d0a0c95e2859181714a23c4eb49
