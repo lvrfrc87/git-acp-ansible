29d59232b5a6dde5cbde8897832c5830ca219479
Federico87 <15066806+lvrfrc87@users.noreply.github.com>
2023-05-30 18:03:56 +0000

    test

--- deployment/dev.sh
+++ deployment/dev.sh
@@ -3,3 +3,4 @@ cd ../ansible_collections/lvrfrc87/git_acp/ && \
 ansible-test sanity --docker -v plugins/ && \
 ansible-test integration -v --docker && \
 ansible-lint tests/integration/targets/git_acp/tasks/main.yml
+This is a line
